b43c31781f666314aa71e71cd8bcd915039276b1
ihatepineapple <ihatepineapple@gmail.com>
2021-12-27 12:26:36 +0100

    tasks rendering from data ok

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
-import React, { useState } from 'react';
-import data from './data.json';
+import React from 'react';
+
+import Todo from './components/todo';
 
 
-
-function App() {
-  
-  }
+function App(props) {
+  console.log(props)
+  const taskList = props.tasks.map(task => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.complete}
+      key={task.id}
+    />
+  ));
 
   return (
-    <div className="App">
-
-          
-
+    <div className="todoapp stack-large">
+      <h1>Todo App</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            What needs to be done?
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+       
+      </ul>
     </div>
   );
 }
